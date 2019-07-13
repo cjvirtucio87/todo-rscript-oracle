287198a7b88fe8c2ecf7be79707d17c2eb134a1e
CJ Virtucio <cjv287@gmail.com>
2019-07-13 13:00:23 -0400

    now using orangehrm/oracle-xe-11g

--- init.sh
+++ init.sh
@@ -3,6 +3,11 @@
 readonly ROOT_DIR="$(dirname "$(readlink -f "$0")")";
 readonly ORACLE_IMAGE_NAME="${ORACLE_IMAGE_NAME:-orangehrm/oracle-xe-11g}";
 readonly ORACLE_IMAGE_TAG="${ORACLE_IMAGE_TAG:-latest}";
+readonly ORACLE_PASSWORD="${ORACLE_PASSWORD:-oracle}";
+readonly ORACLE_SID="${ORACLE_SID:-xe}";
+readonly ORACLE_PORT="${ORACLE_PORT:-1521}";
+readonly ORACLE_SHM_SIZE="${ORACLE_SHM_SIZE:-2g}";
+readonly ORACLE_USER="${ORACLE_USER:-system}";
 
 cleanup() {
   local network_name=$1;
@@ -15,31 +20,6 @@ cleanup() {
   done
 
   docker network rm "${network_name}";
-}
-
-start_dependency_service() {
-  local service_name=$1;
-  shift;
-  local network_name=$1;
-  shift;
-  local image_name=$1;
-  shift;
-
-  echo "starting service: ${service_name}";
-
-  # shellcheck disable=SC2154
-  docker run \
-    --detach \
-    --interactive \
-    --tty \
-    --rm \
-    --network "${network_name}" \
-    --env "http_proxy=${http_proxy}" \
-    --env "https_proxy=${https_proxy}" \
-    --env "no_proxy=${no_proxy}" \
-    --name "${service_name}" \
-    "${image_name}" \
-    "$@";
 }
 
 main() {
@@ -64,26 +44,31 @@ main() {
     docker image rm "${runner_image_name}";
   fi
 
-  if [[ -n "$(docker images ${runner_image_name} --format "{{.ID}}")" ]]; then
-    echo "Docker image ${runner_image_name} already exists; skipping creation";
-  else
-    # shellcheck disable=SC2154
-    docker build \
-      --build-arg "http_proxy=${http_proxy}" \
-      --build-arg "https_proxy=${https_proxy}" \
-      --build-arg "no_proxy=${no_proxy},${hashed_name}" \
-      --network "${network_name}" \
-      -t "${runner_image_name}" \
-      -f "${ROOT_DIR}/.dockerfile/Dockerfile" \
-      .
-  fi
+  # shellcheck disable=SC2154
+  docker build \
+    --build-arg "http_proxy=${http_proxy}" \
+    --build-arg "https_proxy=${https_proxy}" \
+    --build-arg "no_proxy=${no_proxy},${hashed_name}" \
+    --network "${network_name}" \
+    -t "${runner_image_name}" \
+    -f "${ROOT_DIR}/.dockerfile/Dockerfile" \
+    .
 
   if [[ -n "$(docker container ps -af "name=${oracle_name}" --format '{{.ID}}')" ]]; then
     echo "service ${oracle_name} already exists; skipping creation";
   else
-    start_dependency_service "${oracle_name}" "${network_name}" "${ORACLE_IMAGE_NAME}:${ORACLE_IMAGE_TAG}";
+    echo 'running oracle container';
+    docker run \
+      --tty \
+      --detach \
+      --rm \
+      --network "${network_name}" \
+      --shm-size="${ORACLE_SHM_SIZE}" \
+      --name "${oracle_name}" \
+      "${ORACLE_IMAGE_NAME}:${ORACLE_IMAGE_TAG}" 
   fi
 
+  echo 'running runner container';
   # shellcheck disable=SC2154
   docker run \
     --interactive \
@@ -96,7 +81,7 @@ main() {
     --env "ORACLE_TIMEOUT=60" \
     --env "ORACLE_HOST=${oracle_name}" \
     --env "ORACLE_PORT=${ORACLE_PORT}" \
-    --env "ORACLE_DB=${ORACLE_DB}" \
+    --env "ORACLE_DB=${ORACLE_SID}" \
     --env "ORACLE_USER=${ORACLE_USER}" \
     --env "ORACLE_PASSWORD=${ORACLE_PASSWORD}" \
     --name "${runner_name}" \
