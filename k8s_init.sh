4a0fd436a24f1a4711217dd9f82fe5e882949ad3
CJ Virtucio <cjv287@gmail.com>
2019-08-08 09:07:24 -0400

    added initial k8s_init.sh

---
+++ k8s_init.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+readonly ROOT_DIR="$(dirname "$(readlink -f "$0")")";
+readonly APP_ORACLE_IMAGE_NAME="${APP_ORACLE_IMAGE_NAME:-orangehrm/oracle-xe-11g}";
+readonly APP_ORACLE_IMAGE_TAG="${APP_ORACLE_IMAGE_TAG:-latest}";
+readonly APP_ORACLE_PASSWORD="${APP_ORACLE_PASSWORD:-oracle}";
+readonly APP_ORACLE_SID="${APP_ORACLE_SID:-xe}";
+readonly APP_ORACLE_PORT="${APP_ORACLE_PORT:-1521}";
+readonly APP_ORACLE_REQUESTS="${APP_ORACLE_REQUESTS:-cpu=1,memory=2G}";
+readonly APP_ORACLE_USER="${APP_ORACLE_USER:-system}";
+
+main() {
+  local name='todo';
+  local oracle_name="${name}-oracle";
+  local runner_name="${name}-runner";
+  local runner_image_name="cjvirtucio87/${runner_name}";
+  local runner_image_version='latest';
+
+  # shellcheck disable=SC2154
+  docker build \
+    --build-arg "http_proxy=${http_proxy}" \
+    --build-arg "https_proxy=${https_proxy}" \
+    --build-arg "no_proxy=${no_proxy}" \
+    -t "${runner_image_name}:${runner_image_version}" \
+    -f "${ROOT_DIR}/.dockerfile/Dockerfile" \
+    .
+
+  echo 'running oracle container';
+  oc run "${oracle_name}" \
+    --image="${APP_ORACLE_IMAGE_NAME}:${APP_ORACLE_IMAGE_TAG}" \
+    --requests="${APP_ORACLE_REQUESTS}" \
+    --port=1521 \
+    --generator=run/v1 \
+    "${APP_ORACLE_IMAGE_NAME}:${APP_ORACLE_IMAGE_TAG}"
+}
+
+main "$@";
