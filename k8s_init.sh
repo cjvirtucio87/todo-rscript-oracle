5abcc31e30d52f2549f7290475445c5c873605b8
CJ Virtucio <cjv287@gmail.com>
2019-08-10 11:16:42 -0400

    added labels and exposing service (todo: need to figure out why I still cant connect)

--- k8s_init.sh
+++ k8s_init.sh
@@ -12,6 +12,7 @@ readonly APP_ORACLE_USER="${APP_ORACLE_USER:-system}";
 main() {
   local name='todo';
   local oracle_name="${name}-oracle";
+  local oracle_selector="${oracle_name}=default";
   local runner_name="${name}-runner";
   local runner_image_name="cjvirtucio87/${runner_name}";
   local runner_image_version='latest';
@@ -29,9 +30,16 @@ main() {
   oc run "${oracle_name}" \
     --image="${APP_ORACLE_IMAGE_NAME}:${APP_ORACLE_IMAGE_TAG}" \
     --requests="${APP_ORACLE_REQUESTS}" \
+    --labels="${oracle_selector}" \
+    --replicas=3 \
     --port=1521 \
     --generator=run/v1 \
     "${APP_ORACLE_IMAGE_NAME}:${APP_ORACLE_IMAGE_TAG}"
+
+  oc expose rc "${oracle_name}" \
+    --selector="${oracle_selector}" \
+    --port="${APP_ORACLE_PORT}" \
+    --target-port="${APP_ORACLE_PORT}";
 }
 
 main "$@";
