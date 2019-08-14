4c9c16322328b02a5afbf3da32a9ad4d0f6b0167
CJ Virtucio <cjv287@gmail.com>
2019-08-14 09:27:52 -0400

    now creating bc

--- k8s_init.sh
+++ k8s_init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 readonly ROOT_DIR="$(dirname "$(readlink -f "$0")")";
+readonly KUBE_DIR="${ROOT_DIR}/.kube";
 readonly APP_ENV='dev';
 readonly APP_ORACLE_IMAGE_NAME="${APP_ORACLE_IMAGE_NAME:-orangehrm/oracle-xe-11g}";
 readonly APP_ORACLE_IMAGE_TAG="${APP_ORACLE_IMAGE_TAG:-latest}";
@@ -16,44 +17,53 @@ main() {
   local oracle_pod_name="${name}-oracle-pod";
   local oracle_rc_name="${name}-oracle-rc";
   local runner_name="${name}-runner";
+  local runner_build_name="${name}-runner-build";
   local runner_pod_name="${name}-runner-pod";
   local runner_job_name="${name}-runner-job";
-  local runner_image_name="cjvirtucio87/${runner_name}";
+  local runner_image_name="${runner_name}";
   local runner_image_version='latest';
 
-  # shellcheck disable=SC2154
-  docker build \
-    --build-arg "http_proxy=${http_proxy}" \
-    --build-arg "https_proxy=${https_proxy}" \
-    --build-arg "no_proxy=${no_proxy}" \
-    -t "${runner_image_name}:${runner_image_version}" \
-    -f "${ROOT_DIR}/.dockerfile/Dockerfile" \
-    .
-
-  echo 'running oracle controller';
-  cat "${ROOT_DIR}/.kube/oracledb.yml" \
-    | oracle_rc_name="${oracle_rc_name}" \
-      oracle_pod_name="${oracle_pod_name}" \
+  echo 'creating build config';
+  cat "${KUBE_DIR}/runner_build.yml" \
+    | runner_build_name="${runner_build_name}" \
       env="${APP_ENV}" \
-      oracle_image="${APP_ORACLE_IMAGE_NAME}:${APP_ORACLE_IMAGE_TAG}" \
-      oracle_name=${oracle_name} \
-      oracle_user="${APP_ORACLE_USER}" \
-      oracle_pw="${APP_ORACLE_PASSWORD}" \
-      oracle_host="localhost" \
-      oracle_port="${APP_ORACLE_PORT}" \
-      oracle_db="${APP_ORACLE_DB}" \
-      envsubst \
-    | oc create -f -
-
-  echo 'running runner controller';
-  cat "${ROOT_DIR}/.kube/runner.yml" \
-    | runner_job_name="${runner_job_name}" \
-      runner_pod_name="${runner_pod_name}" \
-      env="${APP_ENV}" \
+      http_proxy="${http_proxy}" \
+      https_proxy="${https_proxy}" \
+      no_proxy="${no_proxy}" \
+      app_oracle_host="${oracle_name}" \
+      app_oracle_port="${APP_ORACLE_PORT}" \
+      app_oracle_db="${APP_ORACLE_SID}" \
+      app_oracle_user="${APP_ORACLE_USER}" \
+      app_oracle_password="${APP_ORACLE_PASSWORD}" \
+      dockerfile_path=".dockerfile/Dockerfile" \
       runner_image="${runner_image_name}:${runner_image_version}" \
-      runner_name=${runner_name} \
       envsubst \
     | oc create -f -
+
+#  echo 'running oracle controller';
+#  cat "${ROOT_DIR}/.kube/oracledb.yml" \
+#    | oracle_rc_name="${oracle_rc_name}" \
+#      oracle_pod_name="${oracle_pod_name}" \
+#      env="${APP_ENV}" \
+#      oracle_image="${APP_ORACLE_IMAGE_NAME}:${APP_ORACLE_IMAGE_TAG}" \
+#      oracle_name=${oracle_name} \
+#      oracle_user="${APP_ORACLE_USER}" \
+#      oracle_pw="${APP_ORACLE_PASSWORD}" \
+#      oracle_host="localhost" \
+#      oracle_port="${APP_ORACLE_PORT}" \
+#      oracle_db="${APP_ORACLE_DB}" \
+#      envsubst \
+#    | oc create -f -
+#
+#  echo 'running runner controller';
+#  cat "${ROOT_DIR}/.kube/runner.yml" \
+#    | runner_job_name="${runner_job_name}" \
+#      runner_pod_name="${runner_pod_name}" \
+#      env="${APP_ENV}" \
+#      runner_image="${runner_image_name}:${runner_image_version}" \
+#      runner_name=${runner_name} \
+#      envsubst \
+#    | oc create -f -
 }
 
 main "$@";
